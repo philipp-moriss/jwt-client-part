a0d13860840b06bf34af50c7caa152ee6f70f4df
philipp-moriss <filverem@gmail.com>
2022-04-12 17:27:03 +0300

    first commit

---
+++ src/components/Preloader/Preloader.module.scss
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Preloader.module.scss'
+
+export const Preloader = () => {
+    return (
+        <div className={styles['preloader-wrapper']}>
+            <div className={styles['preloader']}></div>
+        </div>
+    )
+}
---
+++ src/pages/PreviewSection/PreviewSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Mountains.module.scss'
+
+export const Mountains = () => {
+    return (
+        <section className={styles['']}>
+
+        </section>
+    );
+};
