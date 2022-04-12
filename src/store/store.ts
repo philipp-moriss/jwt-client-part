e3d0793fa94d03ea2661062fe5913e7ae2f58b59
philipp-moriss <filverem@gmail.com>
2022-04-12 19:06:45 +0300

    fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,16 +9,17 @@ import {API_URL} from "../http";
 import {AuthResponse} from "../models/response/AuthResponse";
 import {User} from "../models/User/User";
 
-export default  class Store {
+export default class Store {
     user = {} as User
     isAuth = false
     isLoading = false
     errorText = ''
+
     constructor() {
         makeAutoObservable(this)
     }
 
-    setLoading (bool : boolean) {
+    setLoading(bool: boolean) {
         this.isLoading = bool
     }
 
@@ -26,71 +27,75 @@ export default  class Store {
         this.isAuth = bool
     }
 
-    setUser(user : User) {
+    setUser(user: User) {
         this.user = user
     }
 
-    setError(error : string) {
+    setError(error: string) {
         this.errorText = error
     }
 
-    async login (email : string, password : string) {
+    async login(email: string, password: string) {
         this.setLoading(true)
         try {
-            const {data} = await  AuthService.login(email, password)
-            localStorage.setItem('token',data.accessToken)
+            const {data} = await AuthService.login(email, password)
+            localStorage.setItem('token', data.accessToken)
             this.setAuth(true)
             this.setUser(data.user)
-        }catch (e) {
+        } catch (e) {
             const error = ErrorService.ValidateError(e)
             this.setError(error)
-        }finally {
+        } finally {
             this.setLoading(false)
         }
     }
 
-    async registration (email : string, password : string) {
+    async registration(email: string, password: string) {
         this.setLoading(true)
         try {
             const {data} = await AuthService.registration(email, password)
-            localStorage.setItem('token',data.accessToken)
+            localStorage.setItem('token', data.accessToken)
             this.setAuth(true)
             this.setUser(data.user)
-        }catch (e) {
+        } catch (e) {
             const error = ErrorService.ValidateError(e)
             this.setError(error)
-        }finally {
+        } finally {
             this.setLoading(false)
         }
     }
 
-    async logout () {
+    async logout() {
         this.setLoading(true)
         try {
-            const response = await  AuthService.logout()
+            const response = await AuthService.logout()
             localStorage.removeItem('token')
             this.setAuth(false)
             this.setUser({} as User)
-        }catch (e) {
+        } catch (e) {
             const error = ErrorService.ValidateError(e)
             this.setError(error)
-        }finally {
+        } finally {
             this.setLoading(false)
         }
     }
 
-    async checkAuth () {
+    async checkAuth() {
         this.setLoading(true)
         try {
-            const {data} = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
-            console.log(data)
-            localStorage.setItem('token',data.accessToken)
+            const {data} = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            localStorage.setItem('token', data.accessToken)
             this.setAuth(true)
             this.setUser(data.user)
-        }catch (e) {
+        } catch (e) {
             const error = ErrorService.ValidateError(e)
             this.setError(error)
-        }finally {
+        } finally {
             this.setLoading(false)
         }
     }
